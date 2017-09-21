f7cdc74f6eb4f5723b659cfdba53c92d1924ab07
juaparo <jpmunoz26@misena.edu.co>
2017-09-21 11:04:49 -0500

    Db
    Ultima versión de la db

--- DB/gc (5).sql
+++ DB/gc (5).sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 20-09-2017 a las 17:21:38
--- Versión del servidor: 10.1.26-MariaDB
--- Versión de PHP: 7.1.8
+-- Tiempo de generación: 21-09-2017 a las 17:58:17
+-- Versión del servidor: 10.1.25-MariaDB
+-- Versión de PHP: 5.6.31
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET AUTOCOMMIT = 0;
@@ -113,7 +113,7 @@ END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_ConsultarProgramacion` ()  NO SQL
 BEGIN
-SELECT * FROM tbl_programacioncomite;
+SELECT *FROM tbl_programacioncomite;
 END$$
 
 CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_Consultarproyectos` (IN `fg` INT)  NO SQL
@@ -138,9 +138,9 @@ UPDATE tbl_fichaproyecto SET id_ficha = id, titulo = nom, obj_general = obje, ve
 insert into tbl_dtllproyecto (id_dtllProyecto, Url, id_ficha) VALUES (null,urls,id);
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_EditarProgramacion` (IN `sp_idprogramacion` INT, IN `titulo` VARCHAR(20), IN `sp_fecha` DATE, IN `sp_hora` TIME, IN `sp_lugar` VARCHAR(40))  NO SQL
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_EditarProgramacion` (IN `sp_idprogramacion` INT, IN `sp_fecha` DATE, IN `sp_hora` TIME, IN `sp_lugar` VARCHAR(40))  NO SQL
 BEGIN
-UPDATE tbl_programacioncomite SET tbl_programacioncomite.titulo= titulo,  tbl_programacioncomite.fecha=sp_fecha, tbl_programacioncomite.hora=sp_hora,
+UPDATE tbl_programacioncomite SET tbl_programacioncomite.fecha=sp_fecha, tbl_programacioncomite.hora=sp_hora,
 tbl_programacioncomite.lugar=sp_lugar
 WHERE tbl_programacioncomite.id_programacion=sp_idprogramacion;
 END$$
@@ -262,7 +262,7 @@ INSERT INTO tbl_detallesaprendizproyecto (id_detalle, id_aprendiz, id_ficha) VAL
 UPDATE tbl_aprendiz a set a.estado = 2 where a.id_aprendiz=idapren;
 END$$
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_RegistrarProgramacion` (IN `sp_titulo` VARCHAR(20), IN `sp_fecha` DATE, IN `sp_hora` TIME, IN `sp_lugar` VARCHAR(40))  NO SQL
+CREATE DEFINER=`root`@`localhost` PROCEDURE `sp_RegistrarProgramacion` (IN `sp_titulo` VARCHAR(40), IN `sp_fecha` DATE, IN `sp_hora` TIME, IN `sp_lugar` VARCHAR(40))  NO SQL
 BEGIN
 INSERT INTO tbl_programacioncomite(titulo,fecha,hora,lugar)
 VALUES(sp_titulo,sp_fecha, sp_hora, sp_lugar);
@@ -336,14 +336,11 @@ CREATE TABLE `tbl_comite` (
 
 INSERT INTO `tbl_comite` (`id_comite`, `fk_programacion`, `fk_instructor`) VALUES
 (1, 1, 1),
-(2, 5, 1),
-(3, 5, 2),
-(5, 17, 1),
-(6, 17, 2),
-(7, 17, 3),
-(8, 18, 1),
-(9, 18, 2),
-(10, 18, 3);
+(2, 1, 2),
+(3, 1, 3),
+(4, 13, 1),
+(5, 13, 2),
+(6, 13, 3);
 
 -- --------------------------------------------------------
 
@@ -516,8 +513,22 @@ CREATE TABLE `tbl_programacioncomite` (
 --
 
 INSERT INTO `tbl_programacioncomite` (`id_programacion`, `titulo`, `fecha`, `hora`, `lugar`, `estado`) VALUES
-(17, 'A siasdas', '2017-01-16', '07:00:00', 'hola', 1),
-(18, 'Prueba', '2016-01-01', '01:00:00', 'Torre norte, primer piso', 1);
+(1, '0', '0000-00-00', '01:00:00', '2017-01-01', 1),
+(2, '0', '0000-00-00', '01:00:00', '2017-01-01', 0),
+(3, '0', '0000-00-00', '01:00:00', '2017-01-01', 0),
+(4, '0', '0000-00-00', '01:00:00', '2017-09-22', 0),
+(5, '0', '0000-00-00', '14:00:00', '2017-01-01', 0),
+(6, '0', '0000-00-00', '14:00:00', '2017-01-01', 0),
+(7, '0', '0000-00-00', '14:00:00', '2017-01-01', 0),
+(8, '0', '0000-00-00', '13:01:00', '2019-02-01', 0),
+(9, '0', '0000-00-00', '01:00:00', '2017-02-01', 0),
+(10, '0', '0000-00-00', '01:00:00', '2017-01-01', 0),
+(11, '0', '2019-01-02', '01:00:00', 'sdadasdas', 0),
+(12, '0', '2018-02-02', '13:00:00', 'asaSASA', 0),
+(13, 'SJDASDJASD', '2017-01-01', '14:00:00', 'jdasdjasdas', 1),
+(14, 'SJDASDJASD', '2017-01-01', '14:00:00', 'jdasdjasdas', 0),
+(15, '5515', '2018-01-01', '01:00:00', 'HJYHJH', 0),
+(16, 'qqqq', '2017-01-01', '02:00:00', 'pppp', 0);
 
 --
 -- Índices para tablas volcadas
@@ -618,7 +629,7 @@ ALTER TABLE `tbl_cliente`
 -- AUTO_INCREMENT de la tabla `tbl_comite`
 --
 ALTER TABLE `tbl_comite`
-  MODIFY `id_comite` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+  MODIFY `id_comite` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 --
 -- AUTO_INCREMENT de la tabla `tbl_detallesaprendizgrupo`
 --
@@ -658,7 +669,7 @@ ALTER TABLE `tbl_instructores`
 -- AUTO_INCREMENT de la tabla `tbl_programacioncomite`
 --
 ALTER TABLE `tbl_programacioncomite`
-  MODIFY `id_programacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=19;
+  MODIFY `id_programacion` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
 --
 -- Restricciones para tablas volcadas
 --
